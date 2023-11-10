b8ab8809977bca35fb4c1cea34e518f457cef3e8
Ankur Taxali <ataxali@gmail.com>
2023-11-10 18:49:47 -0500

    Refactor UI elements and reintroduce IS_LOCAL conditional logic.

--- app/components/Tweets.tsx
+++ app/components/Tweets.tsx
@@ -7,20 +7,30 @@ const Tweets = ({ tweets }: { tweets: Tweet[] }) => {
     <div className='tweets'>
       {tweets &&
         tweets.map((tweet) => (
-          <div key={tweet.id} className='tweet'>
-            <div className='tweet-col tweet-col-a'>
-              <div className='tweet-icon'>&#x1F9DA;</div>{' '}
-              {/* Fairy emoji as a placeholder */}
+          <div key={tweet.id}>
+            <div className='aqua-titlebar'>
+              <div className='titlebar-buttons'>
+                <span className='close-button'></span>
+                <span className='minimize-button'></span>
+                <span className='maximize-button'></span>
+              </div>
+              <span className='title'>Test</span>
             </div>
-            <div className='tweet-col tweet-col-b'>
-              <p>{tweet.text}</p>
-              {tweet.message && <p>{tweet.message}</p>}
-              <p>You received {tweet.coins} coins.</p>
-              {tweet.payload && tweet.resource && (
-                <Link to={`/${tweet.resource}/${tweet.payload}`}>
-                  Click here to check it out
-                </Link>
-              )}
+            <div className='tweet'>
+              <div className='tweet-col tweet-col-a'>
+                <div className='tweet-icon'>&#x1F9DA;</div>{' '}
+                {/* Fairy emoji as a placeholder */}
+              </div>
+              <div className='tweet-col tweet-col-b'>
+                <p>{tweet.text}</p>
+                {tweet.message && <p>{tweet.message}</p>}
+                <p>You received {tweet.coins} coins.</p>
+                {tweet.payload && tweet.resource && (
+                  <Link to={`/${tweet.resource}/${tweet.payload}`}>
+                    Click here to check it out
+                  </Link>
+                )}
+              </div>
             </div>
           </div>
         ))}
--- app/routes/workout.$workoutName.tsx
+++ app/routes/workout.$workoutName.tsx
@@ -102,10 +102,10 @@ export const action = async ({ params, request }: LoaderFunctionArgs) => {
       // Look for an env variable called IS_LOCAL
       // if it's true, don't create a setEntry in the database, just return the id 1
       // otherwise, create a new setEntry in the database
-      // if (process.env.IS_LOCAL) {
-      //   console.log('IS_LOCAL is true, not creating a setEntry');
-      //   return json({ setId: 23, coins });
-      // }
+      if (process.env.IS_LOCAL) {
+        console.log('IS_LOCAL is true, not creating a setEntry');
+        return json({ setId: 23, coins });
+      }
 
       const setEntry = await db.setEntry.create({
         data: {
